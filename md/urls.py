affd188e0018b2b498fa41af57cd6bc9d5e5bc36
Brooke <clynch@volute.co>
2016-11-12 16:14:46 -0500

    nav and static files

--- md/urls.py
+++ md/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.contrib.auth import urls as auth_urls
 from kd import views as kd_views
 from django.views.generic import RedirectView
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
